60b597d944c2632724c31dffaafe47bdf74ccd4b
ton1czech <tonyasek007@gmail.com>
2022-01-02 00:56:25 +0100

    🎁NEW: getting manufacter

--- kagar.go
+++ kagar.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
@@ -20,7 +21,7 @@ type Car struct {
 
 func main() {
     links := []string{
-        "https://www.theparking.eu/used-cars/toyota-corolla-e80-ae86.html#!/used-cars/toyota-corolla-e80-ae86.html%3Ftri%3Dprix_croissant",
+        "https://www.theparking.eu/used-cars/toyota-corolla-e80-ae86.html#!/used-cars/toyota-corolla-e80-ae86.html%3Fid_pays%3D1%7C5%26tri%3Dprix_croissant",
         "https://www.theparking.eu/used-cars/toyota-soarer.html#!/used-cars/toyota-soarer.html%3Ftri%3Dprix_croissant",
     }
 
@@ -28,6 +29,8 @@ func main() {
 }
 
 func scrapeWeb(links []string) {
+    cars := []Car{}
+
     c := colly.NewCollector(
         colly.AllowedDomains("www.theparking.eu", "theparking.eu"),
     )
@@ -36,11 +39,15 @@ func scrapeWeb(links []string) {
         panic(err)
     })
 
-    c.OnHTML("ul#resultats", func(h *colly.HTMLElement) {
-        fmt.Println(h)
+    c.OnHTML("div#lists ul#resultats li", func(h *colly.HTMLElement) {
+        doc := h.DOM
+
+        car := Car{}
+        car.Manufacturer = strings.Trim(doc.Find("span.title-block.brand").Text(), " ")
+        cars = append(cars, car)
     })
 
-    for _, link := range links {
-        c.Visit(link)
-    }
+    c.Visit(links[0])
+
+    fmt.Println(cars)
 }
